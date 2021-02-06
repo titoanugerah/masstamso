0a8b04fab0bd0ee71a1be6d30aa1d0f3b2daf867
Tito Anugerah Maharizky <tito.anugerah@epson.co.id>
2021-02-06 15:47:40 +0700

    initial commit

---
+++ frontend/wwwroot/js/Index.js
@@ -0,0 +1,52 @@
+﻿$(document).ready(function () {
+    GetProduct();
+});
+
+$('#language').on('change', function () {
+    console.log('change');
+    $('#currency').val($('#language').val());
+    //$.ajax({
+
+    //    url: 'http://localhost:44354/GetCurrency',
+    //    type: 'GET',
+    //    data: {
+    //        'Id': $('#language').val()
+    //    },
+    //    dataType: 'json',
+    //    success: function (data) {
+    //        alert('Data: ' + data);
+    //    },
+    //    error: function (request, error) {
+    //        alert("Request: " + JSON.stringify(request));
+    //    }
+    //});
+});
+
+function GetCustomer() {
+    $.ajax({
+
+        url: 'http://localhost:44354/api/Customer/Get',
+        type: 'GET',
+        success: function (data) {
+            console.log('customer',data);
+        },
+        error: function (request, error) {
+            alert("Request: " + JSON.stringify(request));
+        }
+    });
+}
+
+function GetProduct() {
+    $.ajax({
+        url: 'https://localhost:44354/api/product',
+        type: 'GET',
+        dataType : 'json',
+        success: function (data) {
+            console.log('product', data);
+        }, error: function (data) {
+            console.log('product', data);
+
+        }
+            
+    });
+}
